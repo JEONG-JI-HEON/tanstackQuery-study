be5ba2e622406f3538fe7c68f9d699a355e0bfe5
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-08-20 17:30:53 +0900

    feat: list title 불러오기

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,7 +1,53 @@
 import React, { useState } from "react";
+import { usePosts } from "../../hook/usePosts";
+import { useQueryClient } from "@tanstack/react-query";
 
-const Posts = () => {
-  return <div>리스트으으으으으으ㅡ</div>;
+const Posts = ({ setPostId }) => {
+  const queryClient = useQueryClient();
+  const { status, data, error, isFetching } = usePosts();
+
+  console.log(data);
+
+  const postTitle = () => {
+    let title;
+    switch (status) {
+      case "pending":
+        title = "loading...";
+        break;
+      case "error":
+        title = <span>Error: {error.message}</span>;
+        break;
+      case "success":
+        title = (
+          <div>
+            {data.map((post) => (
+              <p key={post.id}>
+                <a
+                  onClick={() => setPostId(post.id)}
+                  href="#"
+                  style={
+                    queryClient.getQueryData(["post", post.id])
+                      ? {
+                          fontWeight: "bold",
+                          color: "green",
+                        }
+                      : {}
+                  }
+                >
+                  {post.title}
+                </a>
+              </p>
+            ))}
+          </div>
+        );
+        break;
+      default:
+        break;
+    }
+    return title;
+  };
+
+  return postTitle();
 };
 
 export default Posts;
