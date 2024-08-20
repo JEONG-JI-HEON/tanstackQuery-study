97aacc9aaf969401f24c0698796ee0fb9094ddc1
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-08-20 12:22:42 +0900

    feat: usePosts hook 추가

---
+++ src/hook/usePosts.js
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
+export const usePosts = () => {
+  return useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      return await response.json();
+    },
+  });
+};
