004cd91e41dceb1a783c960da001afb238a4d148
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-08-22 11:58:32 +0900

    #4 style: list 스타일 변경

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { usePosts } from "../../hook/usePosts";
 import { useQueryClient } from "@tanstack/react-query";
 
+import { Avatar, List } from "antd";
+
 const Posts = ({ setPostId }) => {
   const queryClient = useQueryClient();
   const { status, data, error, isFetching } = usePosts();
@@ -19,25 +21,52 @@ const Posts = ({ setPostId }) => {
         break;
       case "success":
         title = (
-          <div>
-            {data.map((post) => (
-              <div key={post.id} onClick={() => setPostId(post.id)}>
-                <p
-                  style={
-                    queryClient.getQueryData(["post", post.id])
-                      ? {
-                          fontWeight: "bold",
-                          color: "green",
-                          cursor: "pointer",
+          // <div>
+          //   {data.map((post) => (
+          //     <div key={post.id} className="cursor-pointer mb-3" onClick={() => setPostId(post.id)}>
+          //       <p
+          //         style={
+          //           queryClient.getQueryData(["post", post.id])
+          //             ? {
+          //                 fontWeight: "bold",
+          //                 color: "red",
+          //               }
+          //             : {}
+          //         }
+          //       >
+          //         {post.title}
+          //       </p>
+          //     </div>
+          //   ))}
+          // </div>
+          <>
+            <List
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={(item, index) => (
+                <List.Item className="cursor-pointer" onClick={() => setPostId(item.id)}>
+                  <List.Item.Meta
+                    avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+                    title={
+                      <p
+                        style={
+                          queryClient.getQueryData(["post", item.id])
+                            ? {
+                                fontWeight: "bold",
+                                color: "red",
+                              }
+                            : {}
                         }
-                      : { cursor: "pointer" }
-                  }
-                >
-                  {post.title}
-                </p>
-              </div>
-            ))}
-          </div>
+                      >
+                        {item.title}
+                      </p>
+                    }
+                    description={item.body}
+                  />
+                </List.Item>
+              )}
+            />
+          </>
         );
         break;
       default:
