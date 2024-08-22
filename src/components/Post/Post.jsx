1a56de657bfe588a3b1d078154df28c89e92afc8
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-08-22 10:02:10 +0900

    #1 feat: list에 해당하는 내용 출력 기능

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,7 +1,39 @@
 import React, { useState } from "react";
+import { usePost } from "../../hook/usePost";
 
-const Post = () => {
-  return <div></div>;
+const Post = ({ postId, setPostId }) => {
+  const { status, data, error, isFetching } = usePost(postId);
+
+  let mainContent;
+  switch (status) {
+    case "pending":
+      mainContent = "로딩중....";
+      break;
+    case "error":
+      mainContent = `에러 코드 ${error}`;
+      break;
+    case "success":
+      mainContent = (
+        <>
+          <h1>{data.title}</h1>
+          <div>
+            <p>{data.body}</p>
+          </div>
+        </>
+      );
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <div className="cursor-pointer" onClick={() => setPostId(-1)}>
+        뒤로가기
+      </div>
+      {mainContent}
+    </div>
+  );
 };
 
 export default Post;
