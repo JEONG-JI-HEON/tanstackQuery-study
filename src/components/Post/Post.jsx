ecebddcc4ca5d268f89850630858d5ce75e036ee
JEONG-JI-HEON <wlgjs0804@naver.com>
2024-08-22 17:42:51 +0900

    #4 style: post skeleton 완료

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,53 +1,105 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePost } from "../../hook/usePost";
 import { HomeOutlined } from "@ant-design/icons";
-import { Card, ConfigProvider } from "antd";
+import { Card, ConfigProvider, Skeleton } from "antd";
 
 const Post = ({ postId, setPostId }) => {
   const { status, data, error, isFetching } = usePost(postId);
 
-  let mainContent;
-  switch (status) {
-    case "pending":
-      mainContent = "로딩중....";
-      break;
-    case "error":
-      mainContent = `에러 코드 ${error}`;
-      break;
-    case "success":
-      mainContent = (
-        <div className="h-full">
-          <ConfigProvider
-            theme={{
-              components: {
-                Card: {
-                  headerFontSize: 36,
-                  headerHeight: 80,
-                },
-              },
-            }}
-          >
+  const [delayedStatus, setDelayedStatus] = useState("pending");
+
+  useEffect(() => {
+    let timer;
+    if (status === "success") {
+      timer = setTimeout(() => {
+        setDelayedStatus("success");
+      }, 500);
+    } else {
+      setDelayedStatus(status);
+    }
+    return () => clearTimeout(timer);
+  }, [status]);
+
+  // let mainContent;
+  // switch (status) {
+  //   case "pending":
+  //     mainContent = "로딩중....";
+  //     break;
+  //   case "error":
+  //     mainContent = `에러 코드 ${error}`;
+  //     break;
+  //   case "success":
+  //     mainContent = (
+  //       <div className="h-full">
+  //         <ConfigProvider
+  //           theme={{
+  //             components: {
+  //               Card: {
+  //                 headerFontSize: 36,
+  //                 headerHeight: 80,
+  //               },
+  //             },
+  //           }}
+  //         >
+  //           <Card
+  //             title={data.title}
+  //             className="border-4 text-2xl h-full"
+  //             extra={<HomeOutlined className="text-2xl text-emerald-400" onClick={() => setPostId(-1)} />}
+  //           >
+  //             <p>{data.body}</p>
+  //           </Card>
+  //         </ConfigProvider>
+  //       </div>
+  //     );
+  //     break;
+  //   default:
+  //     break;
+  // }
+
+  const renderContent = () => {
+    switch (delayedStatus) {
+      case "pending":
+        return (
+          <div className="h-full">
             <Card
-              title={data.title}
+              title={<Skeleton title={false} paragraph={{ rows: 1, width: "70%" }} active />}
               className="border-4 text-2xl h-full"
-              extra={<HomeOutlined className="text-2xl text-emerald-400" onClick={() => setPostId(-1)} />}
             >
-              <p>{data.body}</p>
+              <Skeleton title={false} paragraph={{ rows: 1, width: "70%" }} active />
             </Card>
-          </ConfigProvider>
+          </div>
+        );
+      case "error":
+        return `에러 코드 ${error}`;
+      case "success":
+        return (
+          <div className="h-full">
+            <ConfigProvider
+              theme={{
+                components: {
+                  Card: {
+                    headerFontSize: 36,
+                    headerHeight: 80,
+                  },
+                },
+              }}
+            >
+              <Card
+                title={data.title}
+                className="border-4 text-2xl h-full"
+                extra={<HomeOutlined className="text-2xl text-emerald-400" onClick={() => setPostId(-1)} />}
+              >
+                <p>{data.body}</p>
+              </Card>
+            </ConfigProvider>
+          </div>
+        );
+      default:
+        break;
+    }
+  };
 
-          {/* <h1 className="font-bold text-4xl pb-10">{data.title}</h1>
-          <div>
-            <p>{data.body}</p>
-          </div> */}
-        </div>
-      );
-      break;
-    default:
-      break;
-  }
-
-  return <div className="h-full">{mainContent}</div>;
+  return renderContent();
 };
 
 export default Post;
